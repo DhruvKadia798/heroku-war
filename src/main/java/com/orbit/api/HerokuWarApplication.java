c486540e002470a14a16686e32409754fa80d78c
DhruvKadia798 <dhruvkadia798@gmail.com>
2022-06-11 14:09:18 +0530

    [small fix]

---
+++ src/main/java/com/orbit/api/HerokuWarApplication.java
@@ -0,0 +1,13 @@
+package com.orbit.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuWarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokuWarApplication.class, args);
+	}
+
+}
